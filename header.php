dba068f48ecb03e2a16f4f94382bdafc41415418
shurriklab <vissky1@163.com>
2014-01-07 19:32:45 +0800

    update icon

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+    <link rel="shortcut icon" href="http://bcs.duapp.com/codeshurrik-123/favicon.ico" type="image/x-icon">
 
     <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
 
