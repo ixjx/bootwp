4e14eb50f46f6b47827202c853a2af7dedb4ee88
shurriklab <vissky1@163.com>
2014-01-21 20:15:33 +0800

    move scripts

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
       <footer>
         <p>Copyright &#169;&nbsp;<? the_time('Y');?>&nbsp;<a href="http://shurriklab.github.com"><? bloginfo('name');?></a></p>
         <p>珍爱生命，远离IE。请使用Chrome或Safari，你懂的 :)</p>
+        <p>部分代码和设计风格参考<a href="http://blog.dandyweng.com" target="_blank">Dandy's Blog</a></p>
       </footer>
     </div> <!-- /container -->
 
