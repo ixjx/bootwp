4407fc1a840bcd70006907fb2df748fe0100c59a
shurriklab <vissky1@163.com>
2014-01-28 23:29:37 +0800

    Update header.php

--- header.php
+++ header.php
@@ -14,21 +14,10 @@
     <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
     
     <?php wp_head(); ?>
-<script type="text/javascript">
-    jQuery(document).ready(function($) {
-    /* 存档页面 jQ伸缩 */
-    $('#expand_collapse,.archives-yearmonth').css({cursor:"pointer"});
-    $('#archives ul li ul.archives-monthlisting').hide();
-    $('#archives ul li ul.archives-monthlisting:first').show();
-    $('#archives ul li span.archives-yearmonth').click(function(){$(this).next().slideToggle('fast');return false;});
-    //以下下是全局的操作
-    $('#expand_collapse').toggle(function(){
-      $('#archives ul li ul.archives-monthlisting').slideDown('fast');
-      },function(){
-        $('#archives ul li ul.archives-monthlisting').slideUp('fast');
-      });
-  });
-</script>
+
+    <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
   </head>
 
   <body>
