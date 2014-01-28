b77ec90852bca7bd965decbfa201de2d273e867e
shurriklab <vissky1@163.com>
2014-01-28 23:30:03 +0800

    Update page-archives.php

--- page-archives.php
+++ page-archives.php
@@ -4,6 +4,21 @@ Template Name: Archives
 */
 ?>
 <?php get_header();?>
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
+    /* 存档页面 jQ伸缩 */
+    $('#expand_collapse,.archives-yearmonth').css({cursor:"pointer"});
+    $('#archives ul li ul.archives-monthlisting').hide();
+    $('#archives ul li ul.archives-monthlisting:first').show();
+    $('#archives ul li span.archives-yearmonth').click(function(){$(this).next().slideToggle('fast');return false;});
+    //以下下是全局的操作
+    $('#expand_collapse').toggle(function(){
+      $('#archives ul li ul.archives-monthlisting').slideDown('fast');
+      },function(){
+        $('#archives ul li ul.archives-monthlisting').slideUp('fast');
+      });
+  });
+</script>
 <div class="container distance">
     <div class="row">
       <div class="main">
